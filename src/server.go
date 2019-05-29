d2dbf6e8551fac1df151e36c4d1e117ee10883fd
linyujia <1072308682@qq.com>
2019-05-29 23:45:31 +0200

    server

--- src/server.go
+++ src/server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"net"
 	coap "github.com/lorrin/go-coap"
 )
@@ -10,10 +9,6 @@ func main(){
 	coap.ListenAndServe("udp",":5683",
 		coap.FuncHandler(
 			func(l *net.UDPConn, a *net.UDPAddr, m *coap.Message) *coap.Message{
-				log.Println("---------------------------")
-				//log.Printf("Got message path=%q: %#v from %v", m.Path(), m, a)
-				payload := string(m.Payload)
-				log.Printf("-------Got request message payload:%v", payload)
 				
 				res := &coap.Message{
 				Type:      coap.Acknowledgement,
